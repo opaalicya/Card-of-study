f9a8f96382c653df2cbe4e1b2d2f9ff4bd1f4dd8
Zuzuki :p <142051991+opaalicya@users.noreply.github.com>
2024-10-11 18:33:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'FPA',
+    'o que são no estilo FPA?',
+    'significa First Person Adventure, e consistem em jogos eletrônicos com a perspectiva em primeira pessoa, mas nos quais a mecânica principal não é abater todos os inimigos e sim a exploração do cenário e resolução de quebra-cabeças – elementos que fazem parte de um gênero bastante abrangente e tradicional. Exemplo: Uncharted; Assassin’s Creed'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'MOBA',
+    'o que são jogos MOBA?',
+    'Arena de batalha multijogador em linha é um subgênero de jogos eletrônicos de estratégia em que duas equipes de jogadores competem entre si em um campo de batalha predefinido. Exemplos: Dota 2; League of Legends.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'RPG',
+    'oque são no estilo RPG?',
+    'Role-playing game, também conhecido como RPG, é um tipo de jogo em que os jogadores assumem papéis de personagens e criam narrativas colaborativamente. O progresso de um jogo se dá de acordo com um sistema de regras predeterminado, dentro das quais os jogadores podem improvisar livremente.  Exemplos: Dark Souls; The Witcher.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'MMORPG',
+    'Co que são jogos MMORPG?',
+    'Um jogo de interpretação de personagens online e em massa para multi-jogadores é um tipo de jogo on-line que suporta uma quantidade muito grande de jogadores simultâneos, a qual pode chegar a milhões, além de um ambiente de interação de estado permanente. Exemplo: Lineage II; Guild Wars 2.'
 )
